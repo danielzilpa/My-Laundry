c03400ad8dc65dbac76ff26545093a831d7317ed
danielzilpa <44943578+danielzilpa@users.noreply.github.com>
2019-12-16 17:38:03 +0200

    user and manager home page - design only

---
+++ app/src/main/java/com/beitshean/mylaundry/ManagerHomePageActivity.java
@@ -0,0 +1,14 @@
+package com.beitshean.mylaundry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManagerHomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager_home_page);
+    }
+}
