355398320cdd9e78491fa2751fd8851d4bb86033
danielzilpa <44943578+danielzilpa@users.noreply.github.com>
2019-12-16 19:00:10 +0200

    user new order types - design only

---
+++ app/src/main/java/com/beitshean/mylaundry/UserNewOrderTypesActivity.java
@@ -0,0 +1,14 @@
+package com.beitshean.mylaundry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserNewOrderTypesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_new_order_types);
+    }
+}
