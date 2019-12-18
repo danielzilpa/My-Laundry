59c9431a75ff9a47ca91396e084defe63c1e94c6
danielzilpa <44943578+danielzilpa@users.noreply.github.com>
2019-12-18 11:20:03 +0200

    move to home page according to the user profile

--- app/src/main/java/com/beitshean/mylaundry/User.java
+++ app/src/main/java/com/beitshean/mylaundry/User.java
@@ -5,6 +5,7 @@ public class User {
     public String full_name, email, phone, address;
     public boolean isManager;
 
+
     public User(String full_name, String email, String phone) {
         this.full_name = full_name;
         this.email = email;
